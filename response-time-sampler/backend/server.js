1f071a142ced067b5264a06a7cb583d6aa54177c
Lital Porat <poratlital@gmail.com>
2023-02-28 20:35:33 +0200

    inserting-responses-to-database

--- response-time-sampler/backend/server.js
+++ response-time-sampler/backend/server.js
@@ -1,10 +1,73 @@
+const axios = require("axios");
 const express = require("express");
+const knex = require("./db/db");
 const app = express();
+const list_url = {
+  google: "https://www.google.com/",
+  facebook: "https://www.facebook.com/",
+  twitter: "https://twitter.com/",
+  cnet: "https://www.cnet.com/",
+  amazon: "https://www.amazon.com/",
+};
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.post("/response", (req, res) => {
+  for (const website in list_url) {
+    const url = list_url[website];
+    const startTime = performance.now();
+    axios
+      .get(url)
+      .then(() => {
+        const responseTime = performance.now() - startTime;
+        console.log(
+          `${website}'s response time is ${responseTime} milliseconds`
+        );
+        knex(`${website}_responses`)
+          .insert({
+            response_time: responseTime,
+          })
+          .catch((error) => {
+            console.error(`Error inserting ${website} data: ${error}`);
+          });
+      })
+      .catch((error) => {
+        console.error(`Error fetching data from ${website}: ${error}`);
+      });
+  }
+  res.send("Successfuly added all responses to database");
+});
+
+app.put("/response", (req, res) => {
+  knex("responses")
+    .where("id", 6)
+    .update({ response_time: 5 })
+    .then(() => {
+      knex
+        .select()
+        .from("responses")
+        .then((responses) => {
+          res.send(responses);
+        });
+    });
+});
+
+app.delete("/response", (req, res) => {
+  knex("responses")
+    .where("id", 8)
+    .del()
+    .then(() => {
+      knex
+        .select()
+        .from("responses")
+        .then((responses) => {
+          res.send(responses);
+        });
+    });
 });
 
 app.listen("3000", () => {
