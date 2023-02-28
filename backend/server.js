6e57c907560a6690e781ac0cf7f2bbfc45475812
Lital Porat <poratlital@gmail.com>
2023-02-28 21:52:29 +0200

    backend-fix

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const axios = require("axios");
 const express = require("express");
 const knex = require("./db/db");
 const app = express();
-const list_url = {
+
+const WEBSITES = {
   google: "https://www.google.com/",
   facebook: "https://www.facebook.com/",
   twitter: "https://twitter.com/",
@@ -16,59 +17,73 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.post("/response", (req, res) => {
-  for (const website in list_url) {
-    const url = list_url[website];
+function measureResponse(websiteUrl, websiteName) {
+  return new Promise((resolve, reject) => {
     const startTime = performance.now();
     axios
-      .get(url)
+      .get(websiteUrl)
       .then(() => {
         const responseTime = performance.now() - startTime;
         console.log(
-          `${website}'s response time is ${responseTime} milliseconds`
+          `${websiteName}'s response time is ${responseTime} milliseconds`
         );
-        knex(`${website}_responses`)
-          .insert({
-            response_time: responseTime,
-          })
-          .catch((error) => {
-            console.error(`Error inserting ${website} data: ${error}`);
-          });
+        resolve({ website: websiteName, time: responseTime });
       })
-      .catch((error) => {
-        console.error(`Error fetching data from ${website}: ${error}`);
-      });
+      .catch((e) => reject(`${websiteName}: ${e}`));
+  });
+}
+
+app.post("/response", async (req, res) => {
+  const measurementPromises = [];
+  for (const [websiteName, websiteUrl] of Object.entries(WEBSITES)) {
+    measurementPromises.push(measureResponse(websiteUrl, websiteName));
   }
-  res.send("Successfuly added all responses to database");
+
+  try {
+    const responseTimes = await Promise.all(measurementPromises);
+    const rowsToInsert = responseTimes.map(({ website, time }) => ({
+      website,
+      response_time: time,
+    }));
+    res.json(rowsToInsert);
+    // check
+    // knex(`${websiteName}_responses`)
+    //   .insert(rowsToInsert)
+    //   .catch((error) => {
+    //     console.error(`Error inserting ${websiteName} data: ${error}`);
+    //   });
+  } catch (error) {
+    console.error(`Error fetching data: ${error}`);
+  }
 });
 
-app.put("/response", (req, res) => {
-  knex("responses")
-    .where("id", 6)
-    .update({ response_time: 5 })
-    .then(() => {
-      knex
-        .select()
-        .from("responses")
-        .then((responses) => {
-          res.send(responses);
-        });
-    });
-});
+// app.put("/response", (req, res) => {
+//   knex("responses")
+//     .where("id", 6)
+//     .update({ response_time: 5 })
+//     .then(() => {
+//       knex
+//         .select()
+//         .from("responses")
+//         .then((responses) => {
+//           res.send(responses);
+//         });
+//     });
+// });
 
-app.delete("/response", (req, res) => {
-  knex("responses")
-    .where("id", 8)
-    .del()
-    .then(() => {
-      knex
-        .select()
-        .from("responses")
-        .then((responses) => {
-          res.send(responses);
-        });
-    });
-});
+// app.delete("/response", (req, res) => {
+//   knex("responses")
+//     .where("id", 8)
+//     .del()
+//     .then(() => {
+//       knex
+//         .select()
+//         .from("responses")
+//         .then((responses) => {
+//           res.send(responses);
+//         });
+//     });
+// });
 
 app.listen("3000", () => {
   console.log("Server is runnig on port 3000");
